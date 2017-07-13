7e1f0e8392617977feb37f300274b3e7670303c3
Conor Nosal <cnosal@pivotal.io>
2017-07-13 10:08:10 -0400

    Add oauth

--- components/rest-support/src/main/java/io/pivotal/pal/tracker/restsupport/RestConfig.java
+++ components/rest-support/src/main/java/io/pivotal/pal/tracker/restsupport/RestConfig.java
@@ -11,11 +11,6 @@ import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class RestConfig {
-    @LoadBalanced
-    @Bean
-    public RestOperations restOperations() {
-        return new RestTemplate();
-    }
 
     @Bean
     public ObjectMapper objectMapper() {
